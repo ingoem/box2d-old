14e08752ea1a7057ddb6e09a8170378c37cf7697
udoprog <udoprog@f71193c7-d439-0410-8131-bb32c6e3d2ad>
2009-02-23 16:55:11 +0000

    forgot the config.mk

---
+++ Build/gnumake/config.mk
@@ -0,0 +1,59 @@
+
+#application may set these.
+CXX?=g++
+CC?=gcc
+AR?=ar
+RANLIB?=ranlib
+CXXFLAGS?=-g -O2
+CFLAGS?=-g -O2
+LDFLAGS?=-lm -lgcc
+
+.PHONY: clean-hook toolchain-hook
+.SUFFIXES: .cpp .o .c
+
+PKG_SOURCES=$(addprefix ${PACKAGE}/,${SOURCES})
+COBJECTS=${PKG_SOURCES:.c=.o}
+OBJECTS=${COBJECTS:.cpp=.o}
+
+#how object files are built
+
+# Check the toolchain or die with a verbose error.
+toolchain:
+	@if ! type -f $(CXX) 2>&1 > /dev/null; then echo "!!! Your c++-compiler '$(CXX)' does not exist"; exit 1; fi
+	@if ! type -f $(RANLIB) 2>&1 > /dev/null; then echo "!!! Your ranlib '$(RANLIB)' does not exist"; exit 2; fi
+	@if ! type -f $(AR) 2>&1 > /dev/null; then echo "!!! Your archiver '$(AR)' does not exist"; exit 3; fi
+
+.cpp.o: toolchain
+	@echo "  Compiling: $@"
+	$(CXX) -c ${CXXFLAGS} ${DEFINES} ${INCLUDES} -o $@ $<
+
+.c.o: toolchain
+	@echo "  Compiling: $@"
+	$(CC) -c ${DEFINES} ${INCLUDES} -o $@ $<
+
+shared: ${TARGET}.so
+static: ${TARGET}.a
+binary: ${TARGET}
+
+${TARGET}.a: toolchain ${OBJECTS}
+	@echo "  Creating static library: $@"
+	@mkdir -p $(dir $@)
+	$(AR) cr $@ ${OBJECTS}
+	$(RANLIB) $@
+
+${TARGET}.so: toolchain ${OBJECTS}
+	@echo "  Linking shared library: $@"
+	@mkdir -p $(dir $@)
+	$(CXX) -shared -fPIC ${CXXFLAGS} -C -o $@ ${OBJECTS} ${LDFLAGS}
+
+${TARGET}: toolchain ${OBJECTS}
+	@echo "  Creating binary: $@"
+	@mkdir -p $(dir $@)
+	$(CXX) ${CXXFLAGS} -o $@ ${OBJECTS} ${LDFLAGS}
+
+clean: clean-hook
+	$(RM) ${OBJECTS}
+	$(RM) ${TARGET}.so
+	$(RM) ${TARGET}.a
+	$(RM) ${TARGET}
+	-rmdir $(dir ${TARGET})
